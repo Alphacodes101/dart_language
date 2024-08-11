a408658dee73f1c9f651b697931d87a8b7a8d509
root:$rehyan_yadav <ritulyadav1984@gmail.com>
2024-08-11 20:15:13 +0530

    basic of adart  section maked and add some topics

---
+++ Basics Dart/basics /chap01.dart
@@ -0,0 +1,16 @@
+//* comments  in DARt Langauge
+
+void main() {
+  String str1 = 'this is the single line comment ';
+  String str2 = " this is also a single line comment ";
+  String str3 = ''' this is the multi line 
+  comment ''';
+  String str4 = """ this is also a multi line 
+  comment""";
+  print(str1);
+  print(str2);
+
+  print(str3);
+  print(str4);
+  
+}
---
+++ Basics Dart/basics /chap02.dart
@@ -0,0 +1,9 @@
+// this is the  format of the dart programming language 
+
+void main() {
+
+
+  print("hello world ");
+
+
+}
